34abb9ec077da674a5bbee1120b9d6a2a4b93113
john <john@example.com>
2017-07-02 23:06:45 -0400

    model data comments added

--- src/app/model-data/model-data.component.ts
+++ src/app/model-data/model-data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Person } from './modelclass.component';
+import { Person } from './modelclass.component'; //import Person model from modelclass.component
 
 @Component({
     selector: 'app-model-data',
@@ -8,14 +8,16 @@ import { Person } from './modelclass.component';
 
 })
 export class ModelDataComponent implements OnInit {
-    
+    //person variable used to hold new instance of
     person: Object[];
 
     constructor() { 
+        //instance of person used to hold new instances of Person model
         this.person = [
+            //filling out the new Person models parameters firstname, lastname, occ
             new Person('John', 'Billy', 'Banker'),
             new Person('Sara', 'Jane', 'Delivery'),
-            new Person('Eric', 'Gooding', 'Song Writer'),
+            new Person('Eric', 'Gooding', 'Song Writer')
         ];
     
     }
--- src/app/model-data/modelclass.component.ts
+++ src/app/model-data/modelclass.component.ts
@@ -1,9 +1,10 @@
+//export Person class model
 export class Person {
-
+//using constructor to add model add model values
 constructor(public firstname: string, public lastname: string, public occ:string) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.occ = occ;
+    this.firstname = firstname; //model value firstname
+    this.lastname = lastname; //model value lastname
+    this.occ = occ; //model value occ
   }
 
 
