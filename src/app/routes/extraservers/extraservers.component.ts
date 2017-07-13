f1bdf11f0b4d6c9af3cf4c729c592c4ba09808d5
john <john@example.com>
2017-07-12 23:17:13 -0400

    hostbinding fix

---
+++ src/app/routes/extraservers/extraservers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extraservers',
+  templateUrl: './extraservers.component.html',
+  styleUrls: ['./extraservers.component.css']
+})
+export class ExtraserversComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
