ae345479aed640c9305e84dd1632a1bb566d68d2
Roe Camp <roebklyn22@aol.com>
2018-12-06 13:24:51 -0500

    Update form.component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';//NgForm used to gain local reference to form within ' (ngSubmit) '
 
 @Component({
@@ -14,6 +14,8 @@ export class FormComponent implements OnInit {
 
     ngOnInit() {
     }
+  
+   @ViewChild('f') formaccess: NgForm; //ViewChild() method will give you access to the local reference within the form ('f') then you can access the form with instance 'formaccess'. 
     
     //onSubmit method with form as a paremeter attached to the NgForm module
     onSubmit(form: NgForm){
@@ -21,6 +23,9 @@ export class FormComponent implements OnInit {
         console.log('submitted');
         //will log out the whole form when submit is clicked
         console.log(form);
+      
+        //will also log out the whole form when submit is clicked through the ViewChild method.
+        console.log(this.formaccess)
         
         //the ' reset() ' method on the form alias will clear out the form when the user has finsihed submiting their information.
         form.reset();
