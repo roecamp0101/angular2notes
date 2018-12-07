2ace906739065fb0581ab1133f2271c1d25dff45
Roe Camp <roebklyn22@aol.com>
2018-12-07 12:47:48 -0500

    Update shortenpipe.pipe.ts

--- src/app/pipes/shortenpipe.pipe.ts
+++ src/app/pipes/shortenpipe.pipe.ts
@@ -10,7 +10,8 @@ export class Shortenpipe implements PipeTransform {
     //tranform allows you to transform the data you put into the pipe
     //transform methods has two arguments value and arg
     //limit is a parameter of the value pipe
-    transform(value: any, limit: number): any {
+    //stringAdded is another parameter of the value pipe
+    transform(value: any, limit: number, stringAdded: string): any {
         return value.substr(0, 10) + '...';//substr is the built in javascript method substring. Which limited number of values
         //add + '...' gives a continue '... ' so that it is known that the string has been shorted.
     
@@ -28,9 +29,12 @@ export class Shortenpipe implements PipeTransform {
                                                
                                                
                  /* 
+                 
+                 stringAdded = 'another string added'; //stringAdded another parameter of the value pipe will be intiliazed with 'another string added'
+                 
                  //Adding a parameter to a custom pipe ('limit' is the parameter of the value pipe)
                    if(value.length > limit) {
-                        return value.substr(0, limit) + '...'
+                        return value.substr(0, limit) + '...' + stringAdded; ('stringAdded' is another parameter of the value pipe)
                     }
                     else {
                         return value
