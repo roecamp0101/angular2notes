54edcda0ccb749e6ea6f82b35071390e6a51ec56
Roe Camp <roebklyn22@aol.com>
2018-12-08 09:39:45 -0500

    Update service.logging.ts

--- src/app/service/service.logging.ts
+++ src/app/service/service.logging.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable() /*You use the @Injectable() method when you want to inject a service within a service. ie. 
+if you create service called 'singing.service.ts' and you want to inject the 'Http' service within the singing service. */
 export class Logger {
     
     /*a method that the service has, returing some text 'Some Information from A Service!'*/
